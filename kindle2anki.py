eba053a46ae6e20ca798447d7d54ee07c68ae44c
Francis Perea <francis@francisperea.org>
2020-09-12 18:58:49 +0000

    Versión 1.0 terminada y usable

--- kindle2anki.py
+++ kindle2anki.py
@@ -1,9 +1,21 @@
-import csv
+#!/usr/local/bin/python3
 
-with open('countdown.tsv') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
-	line_count = 0
-	for row in csv_reader:
-		print(f'Inglés: \t{row[0]} \t Uso: {row[1]} ')
-		line_count += 1
-	print(f'Processed {line_count} lines.')
+import csv
+from googletrans import Translator
+
+translator = Translator()
+
+filein=open('countdown.tsv')
+fileout=open('deck.tsv', mode='w')
+
+csv_reader = csv.reader(filein, delimiter='\t')
+csv_writer = csv.writer(fileout, delimiter='\t')
+
+for row in csv_reader:
+	print(f'Creating card: {row[0]}')
+	esword = translator.translate(row[0], src="EN", dest="ES").text
+	csv_writer.writerow([row[0], esword + "<br/><br/>" + row[1]])
+		
+print("\nDeck created")
+filein.close()
+fileout.close()
